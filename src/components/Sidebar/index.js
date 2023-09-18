e2559cc726b87b65334c8869776868e542e86ea6
Steve Hudek <sjhudek@gmail.com>
2023-09-17 22:08:56 -0600

    updated src/components/Sidebar/index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo-sub.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
 
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to='/'>
-            <img src={LogoS} alt="logo" />
+            <img src={LogoS} alt="Logo" />
             <img src={LogoSubtitle} alt="slobodan" />
         </Link>
     </div>

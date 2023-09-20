074dec90a483d8435bbd29156cb9c71acf2daa81
Steve Hudek <sjhudek@gmail.com>
2023-09-19 20:22:47 -0600

    updated src/components/Layout/index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,28 +1,29 @@
+// import { Outlet } from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar';
-import './index.scss';
+import Sidebar from '../Sidebar/'
+import './index.scss'
 
 const Layout = () => {
-    return (
-        <div className='App'>
-            <Sidebar />
-            <div className='page'>
-                <span className='tags top-tags'>
-                    &lt;body&gt;
-                </span>
-                <Outlet />
+  return (
+    <div className='App'>
+      <Sidebar />
+      <div className='page'>
+        <span className='tags top-tags'>
+          &lt;body&gt;
+        </span>
 
-                <span className='tags bottom-tags'>
-                    &lt;/body&gt;
-                    <br />
-                    <span className='bottom-tag-html'>
-                        &lt;/html&gt;
-                    </span>   
-                </span>
+        <Outlet />
 
-            </div>
-        </div>
-    )
+        <span className='tags bottom-tags'>
+          &lt;/body&gt;
+          <br />
+          <span className='bottom-tag-html'>
+          &lt;/html&gt;
+          </span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Layout;

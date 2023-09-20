59be2ab5cc62e4c694c52317feea6b660f54cc54
Steve Hudek <sjhudek@gmail.com>
2023-09-19 20:23:37 -0600

    updated src/components/Sidebar/index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,53 +1,82 @@
-import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faLinkedin,
-    faGithub,
-    // faYoutube,
-    // faSkype,
-  } from '@fortawesome/free-brands-svg-icons'
-  import {
-    faHome,
-    faUser,
-    faEnvelope,
-    // faSuitcase,
-    // faBars,
-    // faClose,
-  } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { 
+    FontAwesomeIcon 
+} from '@fortawesome/react-fontawesome'
+
+import { 
+    faHome, 
+    faUser, 
+    faEnvelope 
+} from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faLinkedinIn,
+  faGithub,
+  // faYoutube,
+  // faSkype,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
-    <div className="nav-bar">
-        <Link className="logo" to='/'>
-            <img src={LogoS} alt="Logo" />
-            <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/stevehudek/">
-                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://github.com/sjhudek">
-                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
-                </a>
-            </li>
-        </ul>
-    </div>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="steve" />
+    </Link>
+    <nav>
+      <NavLink
+        exact='true'
+        activeclassname="active" // Fixed typo here
+        to="/"
+      >
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact='true'
+        activeclassname="active" // Fixed typo here
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact='true'
+        activeclassname="active" // Fixed typo here
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/stevehudek/"
+        >
+          <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />{' '}
+          {/* Updated icon name */}
+        </a>
+      </li>
+
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/sjhudek"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />{' '}
+          {/* Updated icon name */}
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
-export default Sidebar;
+export default Sidebar

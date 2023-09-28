ed130d03a0fe0c6b03d3aaa8261719438db1fdd3
Steve Hudek <sjhudek@gmail.com>
2023-09-27 21:16:32 -0600

    added folder src/components/Contact/

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+
+const Contact = () => {
+
+
+    return (
+
+        <div className="text-zone">
+            
+            <h1>Contact Me</h1>
+                
+        </div>
+
+    )
+};
+
+export default Contact;

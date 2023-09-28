8cef578356e40d3b2aca06cdf113de81b4b8bd33
Steve Hudek <sjhudek@gmail.com>
2023-09-27 21:15:45 -0600

    added folder src/components/About/

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import './index.scss';
+
+const About = () => {
+    
+
+    return (
+
+        <div className="container about-page">
+            <div className="text-zone">
+                <h1>About Steve</h1>
+                <p>Hello! I’m Steve Hudek, Fullstack Web Developer with a penchant for marrying logic with aesthetics. Specializing in creating dynamic and seamless websites, I excel in utilizing a diverse set of technologies, such as JavaScript, React, Node.js, and Python, ensuring an optimal, user-friendly experience. My journey began with a profound interest in the digital realm, slowly evolving into a profession drenched in creativity and precision. .</p>
+                <p>Outside the binary world of zeros and ones, I immerse myself in the boundless universe of art, experimenting with various mediums and techniques. My artistic journey is a fusion of traditional and digital art, creating pieces that resonate with my individuality and interpretation of the world around me. This endeavor is not only a reflection of my passion but also a continual learning process, pushing the boundaries of my creative instincts, and adding a unique perspective to my developmental projects. Every brush stroke is a step towards enriching the visual aspect of my creations, ensuring that they are not only functionally superior but also aesthetically pleasing.</p>
+                <p>Away from the canvas and the coding environment, I have two rescue felines, Jasper and Max. They are my companions in the solitude of creation, offering purrs and playful antics, becoming my source of joy and relaxation. Their playful nature and instinctive curiosity resonate well with my creative pursuits, often serving as delightful muses for my artistic expressions. Whether I am immersed in lines of code or lost in the vibrant swirls of my palette, my feline companions are there, adding an extra layer of warmth and contentment to my diverse world.</p>
+            </div>
+
+        </div>
+    )
+};
+
+export default About;

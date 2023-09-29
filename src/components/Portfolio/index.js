30be24c6da8196b484bc676d4cf756b12e96cf01
Steve Hudek <sjhudek@gmail.com>
2023-09-29 16:11:06 -0600

    updated src/components/Portfolio/index.js

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,22 +1,18 @@
 import './index.scss'
 
 const Portfolio = () => {
+  return (
+    <div className="container portfolio-page">
+      <div className="text-zone">
+        <h1>Recent Projects</h1>
 
-
-    return (
-
-        <div className="text-zone">
-            
-            <h1>Recent Projects</h1>
-
-            <div>
-            <Image src="https://example.com/image.png" alt="My Image" />
-            <Link to="https://example.com/my-link">My Link</Link>
-    </div>
-                
+        <div>
+          <img src="https://example.com/image.png" alt="" />
+          <a href="https://google.com/my-link">My Link</a>
         </div>
+      </div>
+    </div>
+  )
+}
 
-    )
-};
-
-export default Portfolio;
+export default Portfolio

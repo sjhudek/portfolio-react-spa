467b2cdffd221cd3b2dd78109831e5c46a8a590f
Steve Hudek <sjhudek@gmail.com>
2023-09-29 13:47:22 -0600

    added folder src/components/Resume/

---
+++ src/components/Resume/index.js
@@ -0,0 +1,19 @@
+import './index.scss'
+
+const Resume = () => {
+
+
+    return (
+
+        <div className="text-zone">
+            
+            <h1>Resume</h1>
+
+            
+                
+        </div>
+
+    )
+};
+
+export default Resume;

cdc31ecfce2ec1a90823b65a29c26e63b2cd1ef5
Steve Hudek <sjhudek@gmail.com>
2023-09-29 13:38:08 -0600

    updated src/components/Portfolio/index.js

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,22 @@
+import './index.scss'
+
+const Portfolio = () => {
+
+
+    return (
+
+        <div className="text-zone">
+            
+            <h1>Recent Projects</h1>
+
+            <div>
+            <Image src="https://example.com/image.png" alt="My Image" />
+            <Link to="https://example.com/my-link">My Link</Link>
+    </div>
+                
+        </div>
+
+    )
+};
+
+export default Portfolio;

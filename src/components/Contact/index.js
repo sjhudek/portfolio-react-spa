e2ea8386678bc14e2766ccad700d6675660e3c06
Steve Hudek <sjhudek@gmail.com>
2023-09-29 23:19:15 -0600

    src/components/Contact/index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,54 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './index.module.scss';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef(); // Create a ref object
+    
     const [formData, setFormData] = useState({
         name: "",
         email: "",
         message: ""
     });
+    
     const [errors, setErrors] = useState({
         name: null,
         email: null,
         message: null
     });
 
-    const handleChange = (e) => {
+    const handleBlur = (e) => {
         const { name, value } = e.target;
-        setFormData((prev) => ({ ...prev, [name]: value }));
-
-        if (!value) {
+        if (!value.trim()) {
             setErrors((prev) => ({ ...prev, [name]: `${name} is required` }));
         } else {
             setErrors((prev) => ({ ...prev, [name]: null }));
         }
+    };
 
-        if (name === "email" && value) {
-            const emailRegex = /\S+@\S+\.\S+/;
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+
+        if (name === "email" && value.trim()) {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             if (!emailRegex.test(value)) {
                 setErrors((prev) => ({ ...prev, [name]: "Invalid email address" }));
+            } else {
+                setErrors((prev) => ({ ...prev, [name]: null }));
             }
         }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Here, you can handle form submission, for example, send formData to an API.
+        emailjs.sendForm('service_9pdpao4', 'template_d55vjus', form.current, 'PaeAizXk5pcO-CKAe')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
     };
+    
 
     return (
         <div className="container contact-page">
             <div className="text-zone">
                 <h1>Contact Me</h1>
-                <form onSubmit={handleSubmit}>
+                <form ref={form} onSubmit={handleSubmit}>
                     <div>
                         <label>Name</label>
                         <input
                             type="text"
                             name="name"
-                            value={formData.name}
+                            value={formData.name} // dynamic value
+                            placeholder="Your name (required)"
+                            required
                             onChange={handleChange}
-                            onBlur={handleChange} // For validation when cursor moves out of the input field
+                            onBlur={handleBlur}
                         />
                         {errors.name && <span className="error">{errors.name}</span>}
                     </div>
@@ -57,9 +74,11 @@ const Contact = () => {
                         <input
                             type="email"
                             name="email"
-                            value={formData.email}
+                            value={formData.email} // dynamic value
+                            placeholder="yourname@example.com (required)"
+                            required
                             onChange={handleChange}
-                            onBlur={handleChange}
+                            onBlur={handleBlur}
                         />
                         {errors.email && <span className="error">{errors.email}</span>}
                     </div>
@@ -67,13 +86,14 @@ const Contact = () => {
                         <label>Message</label>
                         <textarea
                             name="message"
-                            value={formData.message}
+                            value={formData.message} // dynamic value
+                            placeholder="Send me a message" // placeholder text
                             onChange={handleChange}
-                            onBlur={handleChange}
+                            onBlur={handleBlur}
                         />
                         {errors.message && <span className="error">{errors.message}</span>}
                     </div>
-                    <button type="submit">Submit</button>
+                    <button type="submit">Submit</button> {/* Removed anchor tag */}
                 </form>
             </div>
         </div>

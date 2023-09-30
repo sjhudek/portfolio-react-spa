a99d21d725f6595949a30d3d4b230c39d9408476
Steve Hudek <sjhudek@gmail.com>
2023-09-29 23:20:17 -0600

    updated src/components/Resume/index.js

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,19 +1,15 @@
 import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile } from '@fortawesome/free-solid-svg-icons'; 
 
 const Resume = () => {
-
-
     return (
-
-    <div className="container resume-page">
-        <div className="text-zone">
-            
-            <h1>Resume</h1>
-
-            
-                
+        <div className="container resume-page">
+            <div className="text-zone">
+                <h1>Resume</h1>
+                <FontAwesomeIcon icon={faFile} color="#ffd700" size='5x' />
+            </div>
         </div>
-    </div>  
     )
 };
 

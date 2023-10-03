b839048a2576c9955b8286c986be02021161c47a
karina <kguerrerofernandez@instructors.2u.com>
2023-10-02 23:17:35 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';  
+import Sidebar from './components/Sidebar';  
 import Home from './components/Home';
 import About from './components/About'
 import Contact from './components/Contact';
@@ -9,15 +9,29 @@ import Resume from './components/Resume';
 
 function App() {
   return (
+    <div className='App'>
+      <Sidebar />
+      <div className='page'>
+        <span className='tags top-tags'>
+        {/* &lt;html&gt;<br />
+          &lt;body&gt; */}
+        </span>
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/portfolio' element={<Portfolio />} />
         <Route path='/resume' element={<Resume />} />
-      </Route>
     </Routes>
+    {/* <span className='tags bottom-tags'>
+          &lt;/body&gt;
+          <br />
+          <span className='bottom-tag-html'>
+          &lt;/html&gt;
+          </span>
+        </span> */}
+      </div>
+    </div>
   );
 }
 

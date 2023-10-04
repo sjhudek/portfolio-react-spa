ec74acab37b3f13778f1dd109452249ee7b0ef4d
Steve Hudek <sjhudek@gmail.com>
2023-10-03 18:46:11 -0600

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-
-/* 
-
-(default) slash   /#/  /#/planets
-no slash  /#  /#planets
-hashbang  /#!/  /#!/planets
-
-*/
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+// import { HashRouter } from 'react-router-dom';
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter hashType="hashbang">
+    <BrowserRouter basename={'/portfolio-react-spa'}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()

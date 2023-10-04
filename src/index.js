1702d1b068889e6b60d0f84354332c8016afcfcf
Steve Hudek <sjhudek@gmail.com>
2023-10-03 19:35:31 -0600

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import reportWebVitals from './reportWebVitals'
+// import { BrowserRouter } from 'react-router-dom';
+import App from './App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(
 //   // // {/* <React.StrictMod e> */}
 //   //   <BrowserRouter>
@@ -15,12 +16,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // );
 
 root.render(
-    <BrowserRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-);
+    </React.StrictMode>
+  </HashRouter>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()

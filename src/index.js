096ac91f297e891515426da9cdad5b50c732f220
Steve Hudek <sjhudek@gmail.com>
2023-10-03 19:18:18 -0600

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
-// import { HashRouter } from 'react-router-dom';
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './index.css';
+// Assuming you have these components
+import About from '../src/components/About';
+import Contact from '../src/components/Contact';
+import Portfolio from '../src/components/Portfolio';
+import Resume from '../src/components/Resume';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/portfolio-react-spa'}>
-      <App />
-    </BrowserRouter>
+    <Router basename="/portfolio-react-spa">
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
